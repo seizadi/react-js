fd03a04277de5984320ee56fb4d14e276cbf25fd
Soheil Eizadi <seizadi@infoblox.com>
2020-08-14 23:27:44 -0700

    Updated pics app

---
+++ pics/src/component/ImageCard.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+class ImageCard extends React.Component {
+  imageRef = React.createRef();
+  state = { spans: 0 }
+
+  componentDidMount() {
+    this.imageRef.current.addEventListener('load', this.setSpans);
+  }
+
+  setSpans = () => {
+    const height = this.imageRef.current.clientHeight;
+    // Tracks what you set in ImageCard.css for grid-auto-rows
+    const gridAutoRows = 10;
+    const spans = Math.ceil( height / gridAutoRows +1 );
+    this.setState( { spans: spans });
+  }
+
+  render() {
+    const image = this.props.image;
+    return (
+      <div style={{ gridRowEnd: 'span ' + this.state.spans}}>
+         <img ref={this.imageRef} alt={image.description} src={image.urls.regular} />
+      </div>
+    );
+  }
+}
+
+export default ImageCard;
--- pics/src/component/ImageList.js
+++ pics/src/component/ImageList.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import './ImageList.css';
+import ImageCard from './ImageCard';
 
 const ImageList = (props) => {
+  const images = props.images.map( (image) => {
+    return <ImageCard key={image.id} image={image} />
+  });
+
   return (
-    props.images.map( (image) => {
-      return <img key={image.id} alt={'search'} src={image.urls.regular} />
-    }));
+    <div className={'image-list'} >
+      { images }
+    </div>
+  );
 }
 
 export default ImageList;
